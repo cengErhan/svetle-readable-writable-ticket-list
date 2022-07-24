43cebe1d71a98afc2e716320fc9fdbd76309921d
Erhan Aslan <erhanjava@gmail.com>
2022-07-25 02:08:21 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+import { subscribe } from 'svelte/internal';
+
+  import { readable, writable } from 'svelte/store';
+
+  let ticketList = []
+  var tickets = readable(ticketList);
+
+  function handleClick() {
+    ticketList = [...ticketList, (Math.random().toString(36).substring(7))]
+    tickets = readable(ticketList)
+  }
+</script>
+
+<main>
+  {#each $tickets as $ticket}
+    <p>{$ticket}</p>
+  {/each}
+  <button on:click={handleClick}>create ticket</button>
+</main>
